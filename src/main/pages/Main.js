959aeb815df6037f9a859d192772cd713c3f3b29
sswum <l5450won@naver.com>
2024-08-10 13:29:55 +0900

    위치기반으로 주소뜨게끔 수정해서 다시올림"

--- src/main/pages/Main.js
+++ src/main/pages/Main.js
@@ -5,14 +5,14 @@ const markers = [
   {
     lat: 37.557756188912954,
     lng: 126.94062742683245,
-    info: { content: '<h1>마커안녕</h1>', removable: true },
+    info: { content: '<h1>출력</h1>', removable: true },
   },
   {
     lat: 37.557287959390024,
     lng: 126.94120499658828,
     image:
       'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
-    info: { content: '<h1>마커나와2</h1>', clickable: true }, // 클릭시 토글 형태로 인포윈도우 노출
+    info: { content: '<h1>출력2</h1>', clickable: true }, // 클릭시 토글 형태로 인포윈도우 노출
   },
   { lat: 37.561184514897825, lng: 126.94069261563956 },
 ];
@@ -25,9 +25,9 @@ const options = {
     lng: 126.570667,
   },
   zoom: 3,
-  //marker: markers, //지금 주석처리 안하면 주소기반으로 안 뜨기때문에 일단 수정때문에 주석처리
+  //marker: markers,
   markerImage:
-    'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png', //전체 마커 이미지
+    'https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png',
 };
 
 const Main = () => {
