17376c84b6c13b32185efe3ad98726c177237974
sswum <l5450won@naver.com>
2024-08-12 01:05:18 +0900

    마이페이지 구성 짜고 디자인 시작

--- src/mypage/pages/MypageMain.js
+++ src/mypage/pages/MypageMain.js
@@ -4,7 +4,9 @@ import MemberOnlyContainer from '../../member/containers/MemberOnlyContainer';
 const MypageMain = () => {
   return (
     <MemberOnlyContainer>
-      <h1>마이페이지</h1>
+      <h1>마이페이지 프로필 이미지구간
+        예약내역 리뷰[게시판형태] 프로필이미지 수정
+      </h1>
     </MemberOnlyContainer>
   );
 };
